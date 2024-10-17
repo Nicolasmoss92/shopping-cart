5df32b8705b0b807a9e008708ee31cd9ea594217
00043963 <00043963@randon.com.br>
2024-10-17 16:04:18 -0300

    feat: basic structure page

--- cart/src/components/layout/checkOut/Header/styles.ts
+++ cart/src/components/layout/checkOut/Header/styles.ts
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 export const SubHeaderWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  padding: 10px;
+  padding: 20px 0;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
   font-weight: bold;
   border-bottom: 2px solid #eee;
 `;
