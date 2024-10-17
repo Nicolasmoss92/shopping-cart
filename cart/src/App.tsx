bc821c936038e3dbf8a1933a7100bc062bc5c439
00043963 <00043963@randon.com.br>
2024-10-17 11:07:51 -0300

    feat: CheckOut Sumary

--- cart/src/App.tsx
+++ cart/src/App.tsx
@@ -2,11 +2,12 @@ import Card from "./components/layout/card/Card";
 import Header from "./components/Header/index";
 import SubHeader from "./components/layout/checkOut/Header";
 import ProductItem from "./components/layout/checkOut/ProductItem";
+import CheckoutSummary from "./components/layout/checkOut/CheckoutSummary";
 
 function App() {
   return (
     <div className="App">  
-      <ProductItem />
+      <CheckoutSummary />
     </div>
   );
 }
