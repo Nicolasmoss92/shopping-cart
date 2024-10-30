78decb3b911b017ffb03845c0cc0b2451c69d958
00043963 <00043963@randon.com.br>
2024-10-29 22:32:39 -0300

    feat: initial page

--- cart/src/application/router.tsx
+++ cart/src/application/router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import CheckoutPage from "../pages/CheckOutPage";
 import CardGridPage from "../pages/ListItensPage";
 
@@ -7,6 +7,7 @@ export function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Navigate to="/itens" />} />
                 <Route path="/checkout" element={<CheckoutPage />} />
                 <Route path="/itens/*" element={<CardGridPage />} />
             </Routes>
