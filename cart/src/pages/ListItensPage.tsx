5d37ed9f3f8da1ed595b269bc1668795277301f2
00043963 <00043963@randon.com.br>
2024-10-29 23:53:21 -0300

    feat: imagens project

--- cart/src/pages/ListItensPage.tsx
+++ cart/src/pages/ListItensPage.tsx
@@ -5,13 +5,16 @@ import { CardGrid, MainContainerCheckOutPage, PageWrapper } from "../styles/glob
 import logoSrc from '../assets/images/ROCKETSHOES.png';
 import additionalLogoSrc from '../assets/images/tenis.png';
 const products = [
-  { imageUrl: '/tenis2.jpg', productName: 'Tênis de Caminhada Leve Confortável', productPrice: 'R$100,00' },
-  { imageUrl: '/tenis2.jpg', productName: 'Tênis de Caminhada Leve Confortável', productPrice: 'R$150,00' },
-  { imageUrl: '/tenis2.jpg', productName: 'Tênis de Caminhada Leve Confortável', productPrice: 'R$150,00' },
-  { imageUrl: '/tenis2.jpg', productName: 'Tênis de Caminhada Leve Confortável', productPrice: 'R$150,00' },
-  { imageUrl: '/tenis2.jpg', productName: 'Tênis de Caminhada Leve Confortável', productPrice: 'R$150,00' },
-  { imageUrl: '/tenis2.jpg', productName: 'Tênis de Caminhada Leve Confortável', productPrice: 'R$150,00' },
-  // Adicione mais produtos conforme necessário
+  { imageUrl: "https://droper-media.us-southeast-1.linodeobjects.com/2452023194744615.webp", productName: "Tênis VR Caminhada Confortável Detalhes Couro Masculino", productPrice: "R$453,86" },
+  { imageUrl: "https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/ennqnnblj5irlj6oakba/air-jordan-1-origin-story-release-date.jpg", productName: "Tênis VR Caminhada Confortável Detalhes Couro Masculino", productPrice: "R$340,02" },
+  { imageUrl: "https://droper-media.us-southeast-1.linodeobjects.com/2452023194744615.webp", productName: "Tênis Adidas Duramo Lite 2 0", productPrice: "R$179,12" },
+  { imageUrl: "https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/ennqnnblj5irlj6oakba/air-jordan-1-origin-story-release-date.jpg", productName: "Tênis Adidas Duramo Lite 2 0", productPrice: "R$352,99" },
+  { imageUrl: "https://droper-media.us-southeast-1.linodeobjects.com/2452023194744615.webp", productName: "Tênis VR Caminhada Confortável Detalhes Couro Masculino", productPrice: "R$386,01" },
+  { imageUrl: "https://droper-media.us-southeast-1.linodeobjects.com/2452023194744615.webp", productName: "Tênis VR Caminhada Confortável Detalhes Couro Masculino", productPrice: "R$578,41" },
+  { imageUrl: "https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/ennqnnblj5irlj6oakba/air-jordan-1-origin-story-release-date.jpg", productName: "Tênis de Caminhada Leve Confortável", productPrice: "R$170,39" },
+  { imageUrl: "https://droper-media.us-southeast-1.linodeobjects.com/2452023194744615.webp", productName: "Tênis Adidas Duramo Lite 2 0", productPrice: "R$448,61" },
+  { imageUrl: "https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/ennqnnblj5irlj6oakba/air-jordan-1-origin-story-release-date.jpg", productName: "Tênis VR Caminhada Confortável Detalhes Couro Masculino", productPrice: "R$512,65" },
+  { imageUrl: "https://droper-media.us-southeast-1.linodeobjects.com/2452023194744615.webp", productName: "Tênis VR Caminhada Confortável Detalhes Couro Masculino", productPrice: "R$276,22" }
 ];
 
 const CardGridPage: React.FC = () => {
